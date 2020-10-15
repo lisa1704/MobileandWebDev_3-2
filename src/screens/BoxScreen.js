67b81f6771481a5cac0e8c0d2c8059415b59df17
Lisa Chowdhury <47085691+lisa1704@users.noreply.github.com>
2020-10-15 23:00:28 +0600

    #useState variable used on BoxScreen

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -1,18 +1,29 @@
 import React, { useState } from "react";
 import { Button, View, StyleSheet } from "react-native";
 
+const createRGB = () => {
+    let red = Math.floor(Math.random() * 255);
+    let green = Math.floor(Math.random() * 255);
+    let blue = Math.floor(Math.random() * 255);
+
+    let str = "rgb(" + red + "," + green + "," + blue+")";
+    return str;
+};
+
 const BoxScreen = () => {
+    let [Color, setColor] = useState("rgb(0,255,0)");
     return (
         <View>
             <Button
                 title="Change color"
                 onPress={
                     function () {
-
+                        setColor(createRGB());
+                        console.log(createRGB());
                     }
                 }
             />
-            <View style={{ width: 100, height: 100, backgroundColor: "rgb(0,255,0)" }} />
+            <View style={{ width: 100, height: 100, backgroundColor: Color }} />
 
         </View>
     );
