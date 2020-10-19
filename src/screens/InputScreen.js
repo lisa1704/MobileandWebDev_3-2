9b28f7b59113dd5e6a122975f9b40ffd3de56cd4
Lisa Chowdhury <47085691+lisa1704@users.noreply.github.com>
2020-10-19 21:45:25 +0600

    #Picker added to Input Screen

--- src/screens/InputScreen.js
+++ src/screens/InputScreen.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import { Text, Button, View, StyleSheet, TextInput, Switch } from "react-native";
-
+import { Picker } from "@react-native-community/picker";
 const InputScreen = () => {
     let [username, setusername] = useState("");
     let [isenabled, setisenabled] = useState(false);
+    let [lang, setLang] = useState('english');
     return (
         <View>
             <Text style={styles.textStyle}>{username}</Text>
@@ -30,6 +31,19 @@ const InputScreen = () => {
                     console.log(isenabled);
                 }}
             />
+            <Picker
+                mode="dropdown"
+                selectedValue={lang}
+                onValueChange={function (itemvalue, itemindex) {
+                    setLang(itemvalue);
+                }}
+            >
+                <Picker.Item label="English" value={"english"} />
+                <Picker.Item label="Spanish" value={"spanish"} />
+                <Picker.Item label="Arabic" value={"arabic"} />
+                <Picker.Item label="German" value={"german"} />
+            </Picker>
+            <Text style={styles.textStyle}>{lang}</Text>
         </View>
         );
 
